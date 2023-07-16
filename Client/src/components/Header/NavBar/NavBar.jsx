9506a8e3d3cbf14fd29b662b27bbc5155c8612b9
Rebeca <becabecks3.gmail.com>
2023-07-16 22:48:48 +0200

    add/fake-login

--- Client/src/components/Header/NavBar/NavBar.jsx
+++ Client/src/components/Header/NavBar/NavBar.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    // const getName = async () => {
+    //   const response = await fetch("/api/user").then(res => console.log(res.json()))
+    //   console.log(response);
+    // }
+    // getName();
+    const fetchUserName = async () => {
+      try {
+       
+          const response = await fetch("/api/user");
+          const data = await response.json();
+          console.log(data)
+          const nombre = data[0].nombre;
+          setUserName(nombre);
+        }
+        
+        catch (error) {console.error("Error al obtener el nombre del usuario:", error);
+      }
+         
+    };
+
+    fetchUserName();
+  }, []);
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          {userName.length && userName ? `Hola, ${userName}` : "Inicio de sesión"}
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
+
