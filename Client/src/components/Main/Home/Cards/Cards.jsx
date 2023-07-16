7c7a05dbe84adf997b18087ba91eaa573a62f3dd
Rebeca <becabecks3.gmail.com>
2023-07-16 22:55:11 +0200

    add/favorites

--- Client/src/components/Main/Home/Cards/Cards.jsx
+++ Client/src/components/Main/Home/Cards/Cards.jsx
@@ -5,6 +5,27 @@ import { v4 as uuidv4 } from "uuid";
 const Cards = ({filter}) => {
   const { eventList } = useContext(EventContext);
 
+
+  const addToFavorites = (eventId) => {
+    fetch('/api/favorites', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ eventId, userId }),
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Guardado correctamente!')
+        } else {
+          console.log('No se ha guardado')
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  };
+  
   return (
     <>
       {eventList.map((event) => (
